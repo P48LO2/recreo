183e77b01d09ededfe024167a41cd1f1f1102fc5
P48LO2 <118339632+P48LO2@users.noreply.github.com>
2023-01-11 11:45:31 +0100

    Segundo commit

--- Documents/NetBeansProjects/prueba/src/prueba/Prueba.java
+++ Documents/NetBeansProjects/prueba/src/prueba/Prueba.java
@@ -15,7 +15,7 @@ public class Prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
+        System.out.println("¿Como te fue todo ayer?");
     }
     
 }
